8a2b58583b37ea044ea128d9790027558172a49f
Alphones <alphonesabhilash@gmail.com>
2025-10-13 18:34:37 +0530

    Added a React Card component with CSS styling

---
+++ Learn-react/src/Card.jsx
@@ -0,0 +1,16 @@
+function Card(){
+    return(
+        <>
+                <div className="card">
+                    <img src="/vite.svg" alt="vite logo" />
+                    <h3>The React</h3>
+                    <p>
+                        React is a JavaScript library. Hopefully I'm correct.
+                        <br />
+                        Currently I'm learning ReactJS.
+                    </p>
+                </div>
+        </>
+    );
+}
+export default Card
