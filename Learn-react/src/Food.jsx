65de514bd1a6b97f3c093f0122e32338cd1ce51d
Alphones <alphonesabhilash@gmail.com>
2025-10-13 18:06:46 +0530

    Added Food Component

---
+++ Learn-react/src/Food.jsx
@@ -0,0 +1,17 @@
+function Food(){
+
+    // const fruits=["Orange", "Apple", "Banana"];
+    const fruit1="Orange";
+    const fruit2="Banana";
+    const fruit3="Apple";
+
+    return(
+        <li>
+            <ol>{fruit1}</ol>
+            <ol>{fruit2}</ol>
+            <ol>{fruit3}</ol>
+        </li>
+    );
+}
+
+export default Food
